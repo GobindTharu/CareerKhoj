f27afe78468caec991eff88b5f1b5a6d4bd77a34
BalGobind Chaudhary <gobind9800@gmail.com>
2025-06-01 22:48:10 +0545

    Jobs Api creation  completed

--- backend/company/company.controller.js
+++ backend/company/company.controller.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { CompanyTable } from "../user/models/company.models.js";
+import { isAuthenticated } from "../user/middleware/user.middleware.js";
 const router = express.Router();
+
 //  register company
-router.post("/company/register", async (req, res) => {
+router.post("/company/register", isAuthenticated, async (req, res) => {
   try {
     const { companyName } = req.body;
     if (!companyName) {
@@ -12,7 +14,7 @@ router.post("/company/register", async (req, res) => {
     }
     let company = await CompanyTable.findOne({ name: companyName });
 
-    if (!company) {
+    if (company) {
       return res
         .status(400)
         .json({ message: "You cannot register same company", success: false });
@@ -26,42 +28,82 @@ router.post("/company/register", async (req, res) => {
       success: true,
     });
   } catch (error) {
-    console.log(error.message);
     return res.status(400).json(error.message);
   }
 });
 
 // get company
-router.post("/company/list", async (req, res) => {
+router.get("/company/get", isAuthenticated, async (req, res) => {
   try {
     const userId = req.id;
-    const companies = await CompanyTable.findOne({ userId });
+    console.log(userId);
+    const companies = await CompanyTable.find({ userId: userId });
+    console.log(companies);
     if (!companies) {
       return res
         .status(404)
         .json({ message: "Companies not found", success: false });
     }
+    return res.status(200).json({ companies, success: true });
   } catch (error) {
-    console.log(error.message);
     return res.status(400).json(error.message);
   }
 });
 
-//
-router.post("/company/list/:id", async (req, res) => {
+// get company by id
+
+router.get("/company/get/:id", isAuthenticated, async (req, res) => {
   try {
     const companyId = req.params.id;
-    const company = await CompanyTable.findOne(companyId);
+    const company = await CompanyTable.findOne({ _id: companyId });
 
     if (!company) {
       return res
         .status(400)
-        .json({ message: "Company not found", success: true });
+        .json({ message: "Company not found", success: false });
     }
+    return res.status(200).json({ company, success: true });
   } catch (error) {
-    console.log(error.message);
     return res.status(400).json(error.message);
   }
 });
-export { router as companyController };
 
+// update company
+router.put("/company/update/:id", isAuthenticated, async (req, res) => {
+  try {
+    const updatedCompany = req.body;
+    const file = req.file;
+
+    const companyId = req.params.id;
+
+    // Check if the name already exists on a different document
+    const existing = await CompanyTable.findOne({
+      name: updatedCompany.name,
+      _id: { $ne: companyId },
+    });
+
+    if (existing) {
+      return res.status(400).json({
+        message: "Company name already exists.",
+        success: false,
+      });
+    }
+    const company = await CompanyTable.findByIdAndUpdate(
+      req.params.id,
+      updatedCompany,
+      { new: true }
+    );
+
+    if (!company) {
+      return res
+        .status(404)
+        .json({ message: "Company not found", success: false });
+    }
+
+    return res.status(200).json({ company, success: true });
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+});
+
+export { router as companyController };
--- backend/user/models/job.models.js
+++ backend/user/models/job.models.js
@@ -6,17 +6,33 @@ const jobSchema = new mongoose.Schema(
       type: String,
       required: [true, "Job title is required"],
       trim: true,
-      minlength: [3, "Job title must be at least 3 characters"],
     },
     description: {
       type: String,
       required: [true, "Job description is required"],
-      minlength: [10, "Job description must be at least 10 characters"],
+    },
+    requirements: {
+      type: [String],
+      required: true,
     },
     salary: {
       type: Number,
       required: [true, "Salary is required"],
       min: [0, "Salary must be a positive number"],
+    },
+    location: {
+      type: String,
+      required: [true, "Location is required"],
+      trim: true,
+    },
+    jobType: {
+      type: String,
+      required: [true, "Job type is required"],
+      enum: ["Full-time", "Part-time", "Internship", "Contract", "Remote"],
+    },
+    positions: {
+      type: Number,
+      required: true,
     },
     category: {
       type: String,
@@ -33,20 +49,7 @@ const jobSchema = new mongoose.Schema(
         "Other",
       ],
     },
-    jobType: {
-      type: String,
-      required: [true, "Job type is required"],
-      enum: ["Full-time", "Part-time", "Internship", "Contract", "Remote"],
-    },
-    positions: {
-      type: Number,
-      required: true,
-    },
-    location: {
-      type: String,
-      required: [true, "Location is required"],
-      trim: true,
-    },
+
     company: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Company", // assuming you have a company model
@@ -54,13 +57,17 @@ const jobSchema = new mongoose.Schema(
     },
     experienceLevel: {
       type: String,
-      enum: ["Fresher", "Mid-Level", "Senior-Level"],
+      enum: [
+        "internship",
+        "Fresher",
+        "Associate",
+        "Mid-Level",
+        "Senior-Level",
+        "Executive",
+      ],
       default: "Fresher",
     },
-    requirements: {
-      type: String,
-      required: true,
-    },
+
     deadline: {
       type: Date,
     },
