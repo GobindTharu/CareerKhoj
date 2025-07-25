efc3a1770dc344df2fd620ec1e7b854af6f8e8be
Susan Gautam <off.susangautam@gmail.com>
2025-07-25 16:18:29 +0545

    Added Resume Templates

--- frontend/src/components/TestResume/Resumedownload.jsx
+++ frontend/src/components/TestResume/Resumedownload.jsx
@@ -1,6 +1,5 @@
 import React, { useRef } from "react";
 import { useReactToPrint } from "react-to-print";
-
 import ClassicTemplate from "./ClassicTemplate";
 import ProfessionalTemplate from "./ProfessionalTemplate";
 import ModernTemplate from "./ModernTemplate";
